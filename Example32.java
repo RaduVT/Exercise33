585c7763cc80243be24505092fee0f0e981ab68f
rvt <alutoa2411@ieselcaminas.org>
2022-09-29 20:28:39 +0200

    Initial commit

---
+++ Example32.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Example33 {
+    public static void main(String[] args){
+        double x,y ,n;
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a number: ");
+
+        n = input.nextDouble();
+
+        x = n;
+        System.out.println(1/2);
+        for (int i =1; i <= 10; i++){
+            y = 1.0 / 2.0* (x + n / x);
+            x = y;
+            System.out.println(x);
+        }
+    }
+}
+
